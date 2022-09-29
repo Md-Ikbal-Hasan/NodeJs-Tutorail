a7a4abd0c2189c3c93933dbfefab9d64e1d86d59
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-09-29 23:23:09 +0600

    package install tutorial done

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+/*
+// import from another js file
 const app = require('./app')
 console.log(app);
 console.log(app.x, app.y, app.z());
+
+// file system
+const fs = require('fs');
+fs.writeFileSync('hello.txt', 'Hello  Ikbal')
+
+const wrtFile = require('fs').writeFileSync;
+wrtFile('test.txt', 'This is for test')
+
+// directory and filename
+console.log(__dirname);
+console.log(__filename);
+*/
+
+/*
+const http = require('http');
+
+http.createServer((req, resp) => {
+    resp.write('<h1> Hello ,  This is Ikbal Hosen </h1>');
+    resp.end();
+
+
+}).listen(4500);
+
+*/
+
+var colors = require('colors')
+console.log("Node package".green);
+
+
+// Importing module
+const chalk = require("chalk");
+
+// Printing the text
+console.log(chalk.red("Ikbal"))
