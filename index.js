dce4db6ef492812dedac74a3772051e8df42bb5f
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-09-30 17:32:41 +0600

    file extension remove

--- index.js
+++ index.js
@@ -4,7 +4,23 @@ const path = require('path');
 const app = express();
 const publicPath = path.join(__dirname, 'public');
 
-app.use(express.static(publicPath))
+// app.use(express.static(publicPath))
 
+app.get('', (req, resp) => {
+    resp.sendFile(`${publicPath}/index.html`)
+})
+
+
+app.get('/about', (req, resp) => {
+    resp.sendFile(`${publicPath}/about.html`)
+})
+
+app.get('/help', (req, resp) => {
+    resp.sendFile(`${publicPath}/help.html`)
+})
+
+app.get('*', (req, resp) => {
+    resp.sendFile(`${publicPath}/404.html`)
+})
 
 app.listen(5000);
