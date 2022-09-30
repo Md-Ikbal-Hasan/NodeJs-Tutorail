e93b72eec092e3dde00d042683418217b7bab64a
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-09-30 16:56:04 +0600

    express js routing

--- index.js
+++ index.js
@@ -5,35 +5,41 @@ app.get('', (req, res) => {
     // console.log("data sent by browser: ", req.query);
     res.send(`
     <h1> Welcome, to Home page</h1> 
-    <a href="/about" >Go to about page  </a>
+    <a href="/about" >Go to about page  </a> <br>
+    <a href="/help" >Go to Help page  </a>
+
     `)
 })
 
 app.get('/about', (req, res) => {
     res.send(`
-    <h1> Welcome, to About page</h1>
+     <h1> Welcome, to About page</h1>
      <input type="text" placeholder = "User name" value=${req.query.name} />
-     <button>Submit </button>
-     <a href="/" >Go to Home page  </a>
+     <button>Submit </button> <br>
+     <a href="/" >Go to Home page  </a> <br>
+     <a href="/help" >Go to Help page  </a>
     
     `)
 })
 
 app.get('/help', (req, res) => {
-    res.send([
-        {
-            name: 'Md Ikbal Hosen',
-            email: 'ikbal@gmail.com'
-        },
-        {
-            name: 'Md Ramjan Hosen',
-            email: 'ramjan@gmail.com'
-        },
-        {
-            name: 'Md Sunny Hosen',
-            email: 'sunny@gmail.com'
-        }
-    ])
+    res.send(
+        [
+            {
+                name: 'Md Ikbal Hosen',
+                email: 'ikbal@gmail.com'
+            },
+            {
+                name: 'Md Ramjan Hosen',
+                email: 'ramjan@gmail.com'
+            },
+            {
+                name: 'Md Sunny Hosen',
+                email: 'sunny@gmail.com'
+            }
+        ]
+
+    )
 })
 
 app.listen(5000)
