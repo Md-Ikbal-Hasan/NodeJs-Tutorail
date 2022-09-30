12ae6db6bed1213d4c2597a9a234cbf1bf437310
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-09-30 08:36:11 +0600

    file creationa and delete with fs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const http = require('http');
 const data = require('./data')
 http.createServer((req, resp) => {
@@ -5,3 +6,17 @@ http.createServer((req, resp) => {
     resp.write(JSON.stringify(data));
     resp.end()
 }).listen(5000)
+
+*/
+
+const fs = require('fs');
+const input = process.argv;
+
+if (input[2] == 'add') {
+    fs.writeFileSync(input[3], input[4])
+} else if (input[2] == 'remove') {
+    fs.unlinkSync(input[3])
+} else {
+    console.log("Invalid input");
+}
+
