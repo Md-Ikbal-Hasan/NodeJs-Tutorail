27cac350d9865eea9f2e5d7f359f10527dd0dd51
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-09-30 10:19:01 +0600

    crud file system added

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const fs = require('fs');
 const path = require('path');
-const dirPath = path.join(__dirname, 'files');
+const dirPath = path.join(__dirname, 'crud');
 
-// crate multiple file under a folder......
-// for (let i = 0; i < 5; i++) {
-//     fs.writeFileSync(`${dirPath}/hello${i}.txt`, `this is simple text file`);
-// }
+// create file...........
+// fs.writeFileSync(`${dirPath}/apple.txt`, `this is simple text file`);
+// fs.writeFileSync(`${dirPath}/test.html`, `this is simple html file`);
 
-fs.readdir(dirPath, (err, files) => {
-    files.forEach(item => {
-        console.log("file name is : ", item);
-    })
+const filePath = `${dirPath}/apple.txt`;
 
+
+// read file  ..........
+// fs.readFile(filePath, 'utf8', (err, item) => {
+//     console.log(item);
+// })
+
+
+// update file ........
+// fs.appendFile(filePath, ' and file name is apple.txt', (err) => {
+//     if (!err) console.log('file is updated');
+// });
+
+
+// rename file ........
+fs.rename(filePath, `${dirPath}/fruit.txt`, (err) => {
+    if (!err) console.log('file name is updated');
 })
+
+// delete file ........
+// fs.unlinkSync(`${dirPath}/fruit.txt`)
