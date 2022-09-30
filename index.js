80ee382365749c16644ff5998671b707e9c214c8
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-09-30 08:10:30 +0600

    api done

--- index.js
+++ index.js
@@ -1,45 +1,7 @@
-/*
-// import from another js file
-const app = require('./app')
-console.log(app);
-console.log(app.x, app.y, app.z());
-
-// file system
-const fs = require('fs');
-fs.writeFileSync('hello.txt', 'Hello  Ikbal')
-
-const wrtFile = require('fs').writeFileSync;
-wrtFile('test.txt', 'This is for test')
-
-// directory and filename
-console.log(__dirname);
-console.log(__filename);
-*/
-
-/*
 const http = require('http');
-
+const data = require('./data')
 http.createServer((req, resp) => {
-    resp.write('<h1> Hello ,  This is Ikbal Hosen </h1>');
-    resp.end();
-
-
-}).listen(4500);
-
-*/
-
-/*
-var colors = require('colors')
-console.log("Node package".green);
-
-
-// Importing module
-const chalk = require("chalk");
-
-// Printing the text
-console.log(chalk.red("Ikbal"))
-
-*/
-
-console.log("try nodemon");
-console.log("try ikbal try and try");
+    resp.writeHead(200, { 'Content-Type': 'application\json' });
+    resp.write(JSON.stringify(data));
+    resp.end()
+}).listen(5000)
