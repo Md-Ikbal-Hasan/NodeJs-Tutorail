2b36fc6cfe281f3e3dc597d122d284817969a394
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-10-01 14:52:49 +0600

    db connected

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const { MongoClient } = require('mongodb');
-const url = 'mongodb://localhost:27017';
-const databaseName = 'e-comm'
-const client = new MongoClient(url);
+const dbConnect = require('./mongodb')
 
-async function getData() {
-    let result = await client.connect();
-    let db = result.db(databaseName);
-    const collection = db.collection('products');
-    let data = await collection.find({}).toArray();
+
+// dbConnect().then((resp) => {
+//     resp.find().toArray().then((data) => {
+//         console.log(data);
+//     })
+// })
+
+
+const main = async () => {
+    let data = await dbConnect();
+    data = await data.find().toArray();
     console.log(data);
+
 }
 
-getData();
+main();
