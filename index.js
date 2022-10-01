89f7b45ec3c178b84c9286ce8788624d3cb2163f
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-10-01 09:15:28 +0600

    middleware added

--- index.js
+++ index.js
@@ -1,41 +1,27 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-const publicPath = path.join(__dirname, 'public');
 
-app.set('view engine', 'ejs')
+const reqFilter = (req, resp, next) => {
+    if (!req.query.age) {
+        resp.send('Please provide age')
+    } else if (req.query.age < 18) {
+        resp.send('you can not access this page')
+    }
+    else {
+        next();
+
+    }
+}
+
+app.use(reqFilter);
+
 
 app.get('', (req, resp) => {
-    resp.sendFile(`${publicPath}/index.html`)
+    resp.send('Welcome to home page')
 })
 
-
-app.get('/about', (req, resp) => {
-    resp.sendFile(`${publicPath}/about.html`)
-})
-
-app.get('/help', (req, resp) => {
-    resp.sendFile(`${publicPath}/help.html`)
-})
-
-
-app.get('/profile', (req, resp) => {
-    const user = {
-        name: 'Md Ikbal Hosen',
-        email: 'ikbal@gmail.com',
-        city: 'Cumilla',
-        skills: ['html', 'css', 'js', 'react', 'node', 'C', 'C++']
-    }
-    resp.render('profile', { user })
-})
-
-app.get('/login', (_, resp) => {
-    resp.render('login')
-})
-
-app.get('*', (req, resp) => {
-    resp.sendFile(`${publicPath}/404.html`)
+app.get('/users', (req, resp) => {
+    resp.send('WElcome to users page')
 })
 
 app.listen(5000);
