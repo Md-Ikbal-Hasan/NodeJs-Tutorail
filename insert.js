02db00174c4ec249ed322843441a0ffb74961f42
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-10-01 16:33:27 +0600

    multiple data inserted

--- insert.js
+++ insert.js
@@ -3,7 +3,10 @@ const dbConnect = require('./mongodb')
 const insert = async () => {
     const db = await dbConnect();
     const result = await db.insert(
-        { name: 'Samsung galaxy', brand: 'Samsung', price: 500, category: 'mobile' }
+        [
+            { name: 'Samsung galaxy', brand: 'Samsung', price: 500, category: 'mobile' },
+            { name: 'redmi note 7 pro', brand: 'xiomi', price: 500, category: 'mobile' },
+        ]
     )
     if (result.acknowledged) {
         console.log('data inserted');
