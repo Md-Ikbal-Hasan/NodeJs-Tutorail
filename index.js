5415e43d2b5ca2af7d25f46608cfce62c4df9e2b
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-10-02 17:14:44 +0600

    schema defined

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ const main = async () => {
     await mongoos.connect("mongodb://localhost:27017/e-comm");
     const ProductsSchema = new mongoos.Schema({
         name: String,
-        price: Number
+        price: Number,
+        brand: String,
+        category: String
     });
 
     const ProductsModel = mongoos.model('products', ProductsSchema);
-    let data = new ProductsModel({ name: "m 12", price: 1200 });
+    let data = new ProductsModel(
+        {
+            name: "Ikbal 12",
+            price: 12900,
+            brand: 'ikbal',
+            category: 'mobile'
+        }
+    );
     let result = await data.save();
     console.log(result);
 }
