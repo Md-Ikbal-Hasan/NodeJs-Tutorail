f041b80b5b0d438cbac0a2fbd07d69a0615ad09f
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-10-02 18:59:29 +0600

    search api added

--- index.js
+++ index.js
@@ -40,6 +40,20 @@ app.put("/update/:_id", async (req, resp) => {
     resp.send(data);
 })
 
+// search api
+app.get("/search/:key", async (req, resp) => {
+    let data = await Product.find(
+        {
+            "$or": [
+                { "name": { $regex: req.params.key } },
+                { "brand": { $regex: req.params.key } }
+            ]
+        }
+    )
+    resp.send(data);
+
+})
+
 
 
 app.listen(5000);
