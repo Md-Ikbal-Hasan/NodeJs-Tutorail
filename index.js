63dadd7154c7d6a3456a40c7775a9ad3bc1bf64c
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-10-02 17:05:17 +0600

    nodejs mongoos added

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-const dbConnect = require('./mongodb')
-
-
-// dbConnect().then((resp) => {
-//     resp.find().toArray().then((data) => {
-//         console.log(data);
-//     })
-// })
-
+const mongoos = require('mongoose');
 
 const main = async () => {
-    let data = await dbConnect();
-    data = await data.find({ brand: 'apple' }).toArray();
-    console.log(data);
+    await mongoos.connect("mongodb://localhost:27017/e-comm");
+    const ProductsSchema = new mongoos.Schema({
+        name: String,
+        price: Number
+    });
 
+    const ProductsModel = mongoos.model('products', ProductsSchema);
+    let data = new ProductsModel({ name: "m 12", price: 1200 });
+    let result = await data.save();
+    console.log(result);
 }
 
 main();
