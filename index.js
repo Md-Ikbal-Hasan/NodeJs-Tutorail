b963954c9f444d44d941a3320761fe235b4754c5
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-10-02 17:40:43 +0600

    crud with nodejs and mongoose

--- index.js
+++ index.js
@@ -1,25 +1,57 @@
-const mongoos = require('mongoose');
+const mongoose = require('mongoose');
 
-const main = async () => {
-    await mongoos.connect("mongodb://localhost:27017/e-comm");
-    const ProductsSchema = new mongoos.Schema({
-        name: String,
-        price: Number,
-        brand: String,
-        category: String
+mongoose.connect('mongodb://localhost:27017/e-comm');
+
+const productSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+    brand: String,
+    category: String
+
+});
+
+const saveInDB = async () => {
+    const Product = mongoose.model('products', productSchema);
+    let data = new Product({
+        name: "max 100",
+        price: 200,
+        brand: 'maxx',
+        category: 'Mobile'
     });
-
-    const ProductsModel = mongoos.model('products', ProductsSchema);
-    let data = new ProductsModel(
-        {
-            name: "Ikbal 12",
-            price: 12900,
-            brand: 'ikbal',
-            category: 'mobile'
-        }
-    );
-    let result = await data.save();
+    const result = await data.save();
     console.log(result);
 }
 
-main();
+// saveInDB()
+
+const updateInDB = async () => {
+    const Product = mongoose.model('products', productSchema);
+    let data = await Product.updateOne(
+        { name: 'victorians' },
+        {
+            $set: { price: 1290 }
+        }
+    )
+    console.log(data);
+
+}
+
+// updateInDB()
+
+const deleteInDB = async () => {
+    const Product = mongoose.model('products', productSchema);
+    let data = await Product.deleteOne({ name: 'victorians pro' });
+    console.log(data);
+
+}
+
+// deleteInDB();
+
+
+const findInDB = async () => {
+    const Product = mongoose.model('products', productSchema);
+    let data = await Product.find({ brand: 'Cumilla' })
+    console.log(data);
+}
+
+// findInDB();
